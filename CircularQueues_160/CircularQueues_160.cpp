6bdf2af9fe67826fd29951c86edf9856cd6d5fa2
enrexl <ersa.aditya.ft22@mail.umy.ac.id>
2023-06-23 11:07:03 +0700

    Menyelesaikan fungsi display

--- CircularQueues_160/CircularQueues_160.cpp
+++ CircularQueues_160/CircularQueues_160.cpp
@@ -69,6 +69,31 @@ public:
             return;
         }
 
+        cout << "\nElement in the queue are...\n";
+
+        //Jika FRONT_position <= REAR_position, iterasi FRONT dan REAR
+        if (FRONT_position <= REAR_position) {
+            while (FRONT_position <= REAR_position) {
+                cout << queue_array[FRONT_position] << "   ";
+                FRONT_position;
+            }
+            cout << endl;
+        }
+        else {//Jika FRONT_position > REAR_position, iterasi dari FRONT hingga akhir array
+            while (FRONT_position <= max - 1) {
+                cout << queue_array[FRONT_position] << "   ";
+                FRONT_position++;
+            }
+
+            FRONT_position = 0;
+
+            //Iterasi dari awal array hingga REAR
+            while (FRONT_position <= REAR_position) {
+                cout << queue_array[FRONT_position] << "   ";
+                FRONT_position++;
+            }
+            cout << endl;
+        }
     }
 };
 int main()
